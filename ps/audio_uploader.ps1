c862bc7d4bb466df35c8445d576aaaa885d120b4
andistuder <andi@cenatus.org>
2026-03-27 12:01:51 +0000

    add powershell script for audio_uploader

---
+++ ps/audio_uploader.ps1
@@ -0,0 +1,55 @@
+<#
+###SETUP###
+
+# Install AWS CLI
+# http://docs.aws.amazon.com/cli/latest/userguide/installing.html
+
+# Configure AWS CLI with credentials:
+# PS> aws configure
+# AWS Access Key ID [****************MQ5A]:
+# AWS Secret Access Key [****************ukte]:
+# Default region name [eu-west-1]:
+# Default output format [None]:
+
+# you may want to reduce maximum number of concurrent requests (default it 10)
+# Lowering this value will make the S3 transfer commands less resource intensive.
+# more info http://docs.aws.amazon.com/cli/latest/topic/s3-config.html
+# PS> aws configure set default.s3.max_concurrent_requests 2
+
+# Test manually, then set up a scheduled task
+# PS> powershell -File C:\path\to\script\audio_uploader.ps1 -AudioDir C:\path\to\audio-dir -S3Loc s3://bucket-name/path/
+
+.SYNOPSIS
+    Uploads audio files to AWS S3.
+
+.DESCRIPTION
+    This script finds .mp3 files in a specified directory, compresses them, uploads them to S3, and then cleans up old backup files.
+
+.PARAMETER AudioDir
+    The directory containing the audio files.
+
+.PARAMETER S3Loc
+    The S3 location (bucket and path) to upload the files to.
+#>
+param (
+    [string]$AudioDir,
+    [string]$S3Loc
+)
+
+$LogFilePath = Join-Path $PSScriptRoot "AudioUploader.log"
+
+"Starting Script $(Get-Date) Source $AudioDir Dest $S3Loc" | Out-File -FilePath $LogFilePath -Append
+
+# uploads all .mp3 files, which have not been changed within the last minutes and renames them to .mp3.bak
+Get-ChildItem -Path "$AudioDir" -Filter *.mp3 -Recurse | Where-Object { $_.LastWriteTime -lt (Get-Date).AddMinutes(-30) } | ForEach-Object {
+    "Start uploading $(Get-Date) File $($_.FullName)" | Out-File -FilePath $LogFilePath -Append
+    aws s3 cp "$($_.FullName)" $S3Loc
+    "Uploading complete $(Get-Date) File $($_.FullName)" | Out-File -FilePath $LogFilePath -Append
+    Rename-Item "$($_.FullName)" "$($_.FullName).bak"
+}
+"Upload complete Source $AudioDir" | Out-File -FilePath $LogFilePath -Append
+# deletes all .bak files, which have not been accessed in the last 7 days.
+Get-ChildItem -Path $AudioDir -Filter *.bak -Recurse | Where-Object { $_.LastWriteTime -lt (Get-Date).AddDays(-7) } | ForEach-Object {
+    Remove-Item $($_.FullName)
+}
+"Script complete $(Get-Date) Source $AudioDir" | Out-File -FilePath $LogFilePath -Append
---
+++ ps/create_scheduled_task_nts2.ps1
@@ -0,0 +1,26 @@
+#Requires -RunAsAdministrator
+
+# Define the name of the scheduled task
+$TaskName = "Audio Uploader NTS2"
+
+# Define the path to the script to be executed
+# $PSScriptRoot is an automatic variable that contains the directory from which a script is being run
+$ScriptPath = Join-Path $PSScriptRoot "audio_uploader.ps1"
+
+$AudioDir = "C:\Users\Radio Production\Recordings\PlayIt\NTS2"
+$S3Loc = "s3://nts-incoming/*_UPLOADS_S2_STREAMING/CompressedOmnia9/"
+
+# Define the action for the scheduled task
+$TaskAction = New-ScheduledTaskAction -Execute "PowerShell.exe" -Argument "-NoProfile -ExecutionPolicy Bypass -File `"$ScriptPath`" -S3Loc `"$S3Loc`" -AudioDir `"$AudioDir`""
+
+# Define the trigger for the scheduled task to run every 15 minutes
+$StartTime = (Get-Date).AddDays(-1).Date
+$TaskTrigger = New-ScheduledTaskTrigger -Once -At $StartTime -RepetitionInterval (New-TimeSpan -Minutes 15)
+
+# Define the settings for the scheduled task
+$TaskSettings = New-ScheduledTaskSettingsSet -AllowStartIfOnBatteries -DontStopIfGoingOnBatteries -StartWhenAvailable -RunOnlyIfNetworkAvailable
+
+# Register the scheduled task
+Register-ScheduledTask -TaskName $TaskName -Action $TaskAction -Trigger $TaskTrigger -Settings $TaskSettings -Description "Runs the audio uploader script every 15 minutes when a network is available." -User "SYSTEM" -Force
+
+Write-Host "Scheduled task '$TaskName' has been created to run '$ScriptPath' every 15 minutes when a network is available."
