ef5678fa258fdc758ae864c295570d37b964de1a
andistuder <andi@cenatus.org>
2026-03-27 12:01:51 +0000

    add powershell script for audio_uploader

---
+++ ps/audio_uploader.ps1
@@ -0,0 +1,58 @@
+<#
+###SETUP###
+
+# Install AWS CLI
+# http://docs.aws.amazon.com/cli/latest/userguide/installing.html
+
+# Configure AWS CLI with credentials:
+# PS> aws configure
+# AWS Access Key ID [****************MQ5A]:
+# AWS Secret Access Key [****************ukte]:
+# Default region name [eu-west-1]:
+# Default output format [None]:
+
+# you may want to reduce maximum number of concurrent requests (default it 10)
+# Lowering this value will make the S3 transfer commands less resource intensive.
+# more info http://docs.aws.amazon.com/cli/latest/topic/s3-config.html
+# PS> aws configure set default.s3.max_concurrent_requests 2
+
+# Test manually, then set up a scheduled task
+# PS> pwsh -File C:\path\to\script\audio_uploader.ps1 -AudioDir C:\path\to\audio-dir -S3Loc s3://bucket-name/path/
+
+.SYNOPSIS
+    Uploads audio files to AWS S3.
+
+.DESCRIPTION
+    This script finds .mp3 files in a specified directory, compresses them, uploads them to S3, and then cleans up old backup files.
+
+.PARAMETER AudioDir
+    The directory containing the audio files.
+
+.PARAMETER S3Loc
+    The S3 location (bucket and path) to upload the files to.
+#>
+param (
+    [string]$AudioDir,
+    [string]$S3Loc
+)
+
+
+# find files where upload was not successful, gunzipping will result in it being scheduled for upload again.
+Get-ChildItem -Path $AudioDir -Filter *.mp3.gz -Recurse | Where-Object { $_.LastWriteTime -lt (Get-Date).AddMinutes(-360) } | ForEach-Object {
+    gzip -d $_.FullName
+}
+
+# uploads all .mp3 files, which have not been changed within the last minutes and renames them to .mp3.bak
+Get-ChildItem -Path $AudioDir -Filter *.mp3 -Recurse | Where-Object { $_.LastWriteTime -lt (Get-Date).AddMinutes(-1) } | ForEach-Object {
+    gzip $_.FullName
+    Get-Date
+    $gzippedFile = "$($_.FullName).gz"
+    aws s3 cp $gzippedFile $S3Loc
+    Get-Date
+    Rename-Item $gzippedFile "$($gzippedFile).bak"
+}
+
+# deletes all .bak files, which have not been accessed in the last 7 days.
+Get-ChildItem -Path $AudioDir -Filter *.bak -Recurse | Where-Object { $_.LastWriteTime -lt (Get-Date).AddDays(-5) } | ForEach-Object {
+    Remove-Item $_.FullName
+}
