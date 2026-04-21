6f855e81d960deb2263180f64c28946608a823b8
andistuder <andi@cenatus.org>
2026-04-21 14:58:04 +0100

    fix path and file name

--- ps/create_scheduled_task.ps1
+++ ps/create_scheduled_task.ps1
@@ -7,7 +7,7 @@ $TaskName = "Audio Uploader"
 # $PSScriptRoot is an automatic variable that contains the directory from which a script is being run
 $ScriptPath = Join-Path $PSScriptRoot "audio_uploader.ps1"
 
-$AudioDir = "C:\Users\Radio Production\Recording\PlayIt\NTS1"
+$AudioDir = "C:\Users\Radio Production\Recordings\PlayIt\NTS1"
 $S3Loc = "s3://nts-incoming/*_UPLOADS_S2_STREAMING/CompressedOmnia9/"
 
 # Define the action for the scheduled task
