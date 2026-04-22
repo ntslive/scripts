a708730a0b84255ff828ddbcd11e46ebd628f6d9
andistuder <andi@cenatus.org>
2026-04-22 09:32:51 +0100

    use transcript

--- ps/test.ps1
+++ ps/test.ps1
@@ -1,5 +1,8 @@
-# Start-Transcript -Path "C:\Logs\ScriptLog.txt" -Append
+param (
+    [string]$LogPath
+)
+Start-Transcript -Path "$LogPath" -Append
     Write-Host "$(Get-Date) Running Test Script"
     Write-Output "$(Get-Date) Running Test Script1"
     "$(Get-Date) Running Test Script1"
-# Stop-Transcript
+Stop-Transcript
