7d3b2fcbc7dde6a51dfa445a82e7083018837bc2
andistuder <andi@cenatus.org>
2026-04-22 09:30:33 +0100

    log options

--- ps/test.ps1
+++ ps/test.ps1
@@ -1,1 +1,5 @@
-Write-Host "$(Get-Date) Running Test Script"
+# Start-Transcript -Path "C:\Logs\ScriptLog.txt" -Append
+    Write-Host "$(Get-Date) Running Test Script"
+    Write-Output "$(Get-Date) Running Test Script1"
+    "$(Get-Date) Running Test Script1"
+# Stop-Transcript
